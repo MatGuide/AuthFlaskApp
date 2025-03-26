6d44ad5c1d4af6f041a228cbcc72eb1066dec5b5
MatGuide <mateusz.gajda.101@gmail.com>
2025-03-26 14:18:07 +0000

    env variables

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite://users.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
